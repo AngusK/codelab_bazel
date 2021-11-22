ffe5cf719482d4a881eb1fc5cfa192a3ca9b8250
AngusK <anguso@gmail.com>
2021-11-22 14:44:45 +0800

    Update Python Proto rules (#35)
    * update proto rules for python
    
    * Update python grpc rules also

--- examples/py/bin.py
+++ examples/py/bin.py
@@ -2,6 +2,9 @@
 """A tiny example binary for the native Python rules of Bazel."""
 from bazel_tutorial.examples.py.lib import GetNumber
 from bazel_tutorial.examples.py.fibonacci.fib import Fib
+from bazel_tutorial.examples.proto.person_pb2 import Person
 
 print("The number is %d" % GetNumber())
 print("Fib(5) == %d" % Fib(5))
+
+addr = Person()
